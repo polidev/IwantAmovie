8d7dc43d6f37b77b20154df96203119c6856b011
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-07-04 10:02:09 -0600

    add: api debounce

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -12,3 +12,34 @@ fetch("https://api.themoviedb.org/3/authentication", options)
   .then((res) => res.json())
   .then((res) => console.log(res))
   .catch((err) => console.error(err));
+
+// ************************************************************ Authentication
+
+export async function getMovies(movieName) {
+  if (!movieName) return null;
+
+  const url =
+    (`https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=1`,
+    options);
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+}
+
+export async function getMovieDetail(movieId) {
+  if (!movieId) return null;
+
+  const url =
+    (`https://api.themoviedb.org/3/movie/${movieId}?language=en-US`, options);
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+}
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+export default function useDebounce(value, delay = 400) {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebounceValue(value), delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debounceValue;
+}
